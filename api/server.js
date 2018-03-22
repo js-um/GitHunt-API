a281739d12d1785dda2e3f66a31ca75252a29a57
johanna <jkgriffin234@gmail.com>
2018-03-21 18:39:38 -0700

    server.js changes for upgrade to 1.0

--- api/server.js
+++ api/server.js
@@ -6,10 +6,10 @@ import cors from 'cors';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import { invert, isString } from 'lodash';
-import { createServer } from 'http';
+// import { createServer } from 'http';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
 import { execute, subscribe } from 'graphql';
-import { Engine } from 'apollo-engine';
+import { ApolloEngine } from 'apollo-engine';
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from './githubKeys';
 
 import { setUpGitHubLogin } from './githubLogin';
@@ -23,68 +23,61 @@ import config from './config';
 
 const WS_GQL_PATH = '/subscriptions';
 
+let engine; 
 // Arguments usually come from env vars
-export function run({ ENGINE_API_KEY, PORT: portFromEnv = 3010 } = {}) {
-  let port = portFromEnv;
-
-  if (isString(portFromEnv)) {
-    port = parseInt(portFromEnv, 10);
-  }
-
-  const wsGqlURL =
-    process.env.NODE_ENV !== 'production'
-      ? `ws://localhost:${port}${WS_GQL_PATH}`
-      : `ws://api.githunt.com${WS_GQL_PATH}`;
+export function run () {
+  const port = 3010;
+  const wsGqlURL = `ws://localhost:${port}${WS_GQL_PATH}`
 
   const app = express();
 
-  if (ENGINE_API_KEY) {
-    const engine = new Engine({
-      engineConfig: {
-        apiKey: ENGINE_API_KEY,
-        stores: [
-          {
-            name: 'publicResponseCache',
-            inMemory: {
-              cacheSize: 10485760,
-            },
-          },
-          {
-            name: 'privateResponseCache',
-            inMemory: {
-              cacheSize: 10485760,
-            },
-          },
-          {
-            name: 'pq',
-            inMemory: {
-              cacheSize: 5000000,
-            },
-          },
-        ],
-        persistedQueries: {
-          store: 'pq',
-        },
-        sessionAuth: {
-          store: 'privateResponseCache',
-          header: 'Authorization',
-        },
-        queryCache: {
-          publicFullQueryStore: 'publicResponseCache',
-          privateFullQueryStore: 'privateResponseCache',
-        },
-        reporting: {
-          debugReports: true,
-        },
-        logging: {
-          level: 'DEBUG',
-        },
-      },
-      graphqlPort: port,
-    });
-    engine.start();
-    app.use(engine.expressMiddleware());
-  }
+  engine = new ApolloEngine({
+    apiKey: 'service:mdg-private-a-service:EB-LWSjPdZX0ph-Yyn2cxA',
+  });
+
+  // if (ENGINE_API_KEY) {
+  //   engine = new ApolloEngine({
+  //     apiKey: 'service:mdg-private-a-service:EB-LWSjPdZX0ph-Yyn2cxA',
+  //     stores: [
+  //       {
+  //         name: 'publicResponseCache',
+  //         inMemory: {
+  //           cacheSize: 10485760,
+  //         },
+  //       },
+  //       {
+  //         name: 'privateResponseCache',
+  //         inMemory: {
+  //           cacheSize: 10485760,
+  //         },
+  //       },
+  //       {
+  //         name: 'pq',
+  //         inMemory: {
+  //           cacheSize: 5000000,
+  //         },
+  //       },
+  //     ],
+  //     persistedQueries: {
+  //       store: 'pq',
+  //     },
+  //     sessionAuth: {
+  //       store: 'privateResponseCache',
+  //       header: 'Authorization',
+  //     },
+  //     queryCache: {
+  //       publicFullQueryStore: 'publicResponseCache',
+  //       privateFullQueryStore: 'privateResponseCache',
+  //     },
+  //     reporting: {
+  //       debugReports: true,
+  //     },
+  //     logging: {
+  //       level: 'DEBUG',
+  //     },
+  //   });
+  // }
+
   app.use(cors());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
@@ -166,7 +159,7 @@ export function run({ ENGINE_API_KEY, PORT: portFromEnv = 3010 } = {}) {
     }
   }
   `,
-    }),
+    })
   );
 
   // Serve our helpful static landing page. Not used in production.
@@ -174,110 +167,117 @@ export function run({ ENGINE_API_KEY, PORT: portFromEnv = 3010 } = {}) {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
 
-  const server = createServer(app);
+  // const server = createServer(app);
 
-  server.listen(port, () => {
+  engine.listen({
+    port,
+    graphqlPaths: ['/graphql'],
+    expressApp: app,
+    launcherOptions: {
+      startupTimeout: 3000,
+    },
+  }, () => {
     console.log(`API Server is now running on http://localhost:${port}`); // eslint-disable-line no-console
     console.log(`API Server over web socket with subscriptions is now running on ws://localhost:${port}${WS_GQL_PATH}`); // eslint-disable-line no-console
   });
 
   // eslint-disable-next-line
-  new SubscriptionServer(
-    {
-      schema,
-      execute,
-      subscribe,
+  // new SubscriptionServer(
+  //   {
+  //     schema,
+  //     execute,
+  //     subscribe,
 
-      // the onOperation function is called for every new operation
-      // and we use it to set the GraphQL context for this operation
-      onOperation: (msg, params, socket) => {
-        return new Promise((resolve) => {
-          if (!config.persistedQueries) {
-            // Get the query, the same way express-graphql does it
-            // https://github.com/graphql/express-graphql/blob/3fa6e68582d6d933d37fa9e841da5d2aa39261cd/src/index.js#L257
-            const { query } = params;
-            if (query && query.length > 2000) {
-              // None of our app's queries are this long
-              // Probably indicates someone trying to send an overly expensive query
-              throw new Error('Query too large.');
-            }
-          }
+  //     // the onOperation function is called for every new operation
+  //     // and we use it to set the GraphQL context for this operation
+  //     onOperation: (msg, params, socket) => {
+  //       return new Promise((resolve) => {
+  //         if (!config.persistedQueries) {
+  //           // Get the query, the same way express-graphql does it
+  //           // https://github.com/graphql/express-graphql/blob/3fa6e68582d6d933d37fa9e841da5d2aa39261cd/src/index.js#L257
+  //           const { query } = params;
+  //           if (query && query.length > 2000) {
+  //             // None of our app's queries are this long
+  //             // Probably indicates someone trying to send an overly expensive query
+  //             throw new Error('Query too large.');
+  //           }
+  //         }
 
-          const gitHubConnector = new GitHubConnector({
-            clientId: GITHUB_CLIENT_ID,
-            clientSecret: GITHUB_CLIENT_SECRET,
-          });
+  //         const gitHubConnector = new GitHubConnector({
+  //           clientId: GITHUB_CLIENT_ID,
+  //           clientSecret: GITHUB_CLIENT_SECRET,
+  //         });
 
-          // Support for persistedQueries
-          if (config.persistedQueries) {
-            // eslint-disable-next-line no-param-reassign
-            params.query = invertedMap[msg.payload.id];
-          }
+  //         // Support for persistedQueries
+  //         if (config.persistedQueries) {
+  //           // eslint-disable-next-line no-param-reassign
+  //           params.query = invertedMap[msg.payload.id];
+  //         }
 
-          let wsSessionUser = null;
-          if (socket.upgradeReq) {
-            const cookies = cookie.parse(socket.upgradeReq.headers.cookie);
-            const sessionID = cookieParser.signedCookie(
-              cookies['connect.sid'],
-              config.sessionStoreSecret,
-            );
+  //         let wsSessionUser = null;
+  //         if (socket.upgradeReq) {
+  //           const cookies = cookie.parse(socket.upgradeReq.headers.cookie);
+  //           const sessionID = cookieParser.signedCookie(
+  //             cookies['connect.sid'],
+  //             config.sessionStoreSecret,
+  //           );
 
-            const baseContext = {
-              context: {
-                Repositories: new Repositories({ connector: gitHubConnector }),
-                Users: new Users({ connector: gitHubConnector }),
-                Entries: new Entries(),
-                Comments: new Comments(),
-              },
-            };
+  //           const baseContext = {
+  //             context: {
+  //               Repositories: new Repositories({ connector: gitHubConnector }),
+  //               Users: new Users({ connector: gitHubConnector }),
+  //               Entries: new Entries(),
+  //               Comments: new Comments(),
+  //             },
+  //           };
 
-            const paramsWithFulfilledBaseContext = Object.assign(
-              {},
-              params,
-              baseContext,
-            );
+  //           const paramsWithFulfilledBaseContext = Object.assign(
+  //             {},
+  //             params,
+  //             baseContext,
+  //           );
 
-            if (!sessionID) {
-              resolve(paramsWithFulfilledBaseContext);
+  //           if (!sessionID) {
+  //             resolve(paramsWithFulfilledBaseContext);
 
-              return;
-            }
+  //             return;
+  //           }
 
-            // get the session object
-            sessionStore.get(sessionID, (err, session) => {
-              if (err) {
-                throw new Error('Failed retrieving sessionID from the sessionStore.');
-              }
+  //           // get the session object
+  //           sessionStore.get(sessionID, (err, session) => {
+  //             if (err) {
+  //               throw new Error('Failed retrieving sessionID from the sessionStore.');
+  //             }
 
-              if (session && session.passport && session.passport.user) {
-                const sessionUser = session.passport.user;
-                wsSessionUser = {
-                  login: sessionUser.username,
-                  html_url: sessionUser.profileUrl,
-                  avatar_url: sessionUser.photos[0].value,
-                };
+  //             if (session && session.passport && session.passport.user) {
+  //               const sessionUser = session.passport.user;
+  //               wsSessionUser = {
+  //                 login: sessionUser.username,
+  //                 html_url: sessionUser.profileUrl,
+  //                 avatar_url: sessionUser.photos[0].value,
+  //               };
 
-                resolve(Object.assign(paramsWithFulfilledBaseContext, {
-                  context: Object.assign(
-                    paramsWithFulfilledBaseContext.context,
-                    {
-                      user: wsSessionUser,
-                    },
-                  ),
-                }));
-              }
+  //               resolve(Object.assign(paramsWithFulfilledBaseContext, {
+  //                 context: Object.assign(
+  //                   paramsWithFulfilledBaseContext.context,
+  //                   {
+  //                     user: wsSessionUser,
+  //                   },
+  //                 ),
+  //               }));
+  //             }
 
-              resolve(paramsWithFulfilledBaseContext);
-            });
-          }
-        });
-      },
-    },
-    {
-      path: WS_GQL_PATH,
-      server,
-    },
-  );
+  //             resolve(paramsWithFulfilledBaseContext);
+  //           });
+  //         }
+  //       });
+  //     },
+  //   },
+  //   {
+  //     path: WS_GQL_PATH,
+  //     server,
+  //   },
+  // );
 
-  return server;
+  // return server;
 }
